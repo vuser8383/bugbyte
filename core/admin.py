0c8f5a3f640fc9099289eaa78b370de2dfe940a4
vuser8383 <vuser8383@gmail.com>
2025-01-12 15:14:04 +0530

    lec-2 completed (models)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Restaurant, Rating, Sale
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Rating)
+admin.site.register(Sale)
